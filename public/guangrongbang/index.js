004a1e176baceb21290bccaf818e2798037e8a67
ApolloChina <959855534@qq.com>
2025-07-18 15:52:31 +0800

    updata shouyi

--- public/guangrongbang/index.js
+++ public/guangrongbang/index.js
@@ -8,9 +8,11 @@ const people = [
     {'name':'依佳','money':0.09},
     {'name':'谢雅蕊','money':0.14},
     {'name':'张赞东','money':1.66},
+    {'name':'陈壮炫','money':1.65},
+    {'name':'其他收益包网站','money':6.7}
 ]
 const update = {
-    'time':'2025年1月31日'
+    'time':'2025年7月18日'
 }
 const updatetime = document.getElementById('updateDate')
 updatetime.innerHTML = '';
--- test.py
+++ test.py
@@ -1,4 +1,14 @@
-m = []
+m = [
+    {'name':'玥涵^.','money':1.12},
+    {'name':'玳霖','money':0.09},
+    {'name':'刘煜南','money':5.05},
+    {'name':'bilibili','money':1.61},
+    {'name':'黄文淼','money':0.01},
+    {'name':'文跃琪','money':0.09},
+    {'name':'依佳','money':0.09},
+    {'name':'谢雅蕊','money':0.14},
+    {'name':'张赞东','money':1.66},
+]
 
 
 while True:
@@ -20,4 +30,4 @@ while True:
         break
 
 for i in m:
-    print(f"    'name':{i['name']},'money':{i['money']},")
+    print(f"    \{'name':'{i['name']}'','money':'{i['money']}'\},")
